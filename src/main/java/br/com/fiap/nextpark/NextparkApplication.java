2cc3a1c0c8fd4e3192eee8376834b1ea8b6014ed
Laboratório FIAP <labsfiap@fiap.com.br>
2025-09-10 22:12:37 -0300

    first commit

---
+++ src/main/java/br/com/fiap/nextpark/NextparkApplication.java
@@ -0,0 +1,13 @@
+package br.com.fiap.nextpark;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NextparkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NextparkApplication.class, args);
+	}
+
+}
